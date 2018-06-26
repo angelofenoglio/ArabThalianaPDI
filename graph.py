f68035f0c3a344582b8c7d64618287c882f267d1
angelofenoglio <angelofenoglio@gmail.com>
2018-06-26 19:37:39 -0300

    Final result

--- graph.py
+++ graph.py
@@ -3,23 +3,28 @@
 import cv2
 import sys
 import numpy
+from os import system
+import os.path
 from collections import deque
 
 
-def neighbours(x,y,image):
+def neighbours(x, y, image):
     img = image
     x_1, y_1, x1, y1 = x-1, y-1, x+1, y+1
     return [ img[x_1][y], img[x_1][y1], img[x][y1], img[x1][y1],
                 img[x1][y], img[x1][y_1], img[x][y_1], img[x_1][y_1] ]
 
-def notSoNeighbours(x,y,image):
+
+def notSoNeighbours(x, y, image):
     img = image
     return [ img[x][y+2], img[x+1][y+2], img[x+2][y+2], img[x+2][y+1], img[x+2][y], img[x+2][y-1], img[x+2][y-2], img[x+1][y-2], img[x][y-2],
              img[x-1][y-2], img[x-2][y-2], img[x-2][y-1], img[x-2][y], img[x-2][y+1], img[x-2][y+2], img[x-1][y+2] ]
 
-def neighboursCoors(x,y):
+
+def neighboursCoors(x, y):
     y, x = x, y
     return [ [y-1, x], [y-1, x+1], [y, x+1], [y+1, x+1], [y+1, x], [y+1, x-1], [y, x-1], [y-1, x-1] ]
+
 
 def transitions(neighbours):
     n = neighbours + neighbours[0:1]
@@ -29,10 +34,10 @@ def transitions(neighbours):
 def getEndPoints(skeleton):
     height, width = skeleton.shape
     endPoints = list()
-    for i in range(1, width - 1):
-        for j in range(1, height - 1):
-            if skeleton[i,j] and numpy.sum(neighbours(i,j,skeleton)) == 1:
-                endPoints.append([i,j])
+    for i in range(1, height - 1):
+        for j in range(1, width - 1):
+            if skeleton[i, j] and numpy.sum(neighbours(i, j, skeleton)) == 1:
+                endPoints.append([i, j])
     return endPoints
 
 
@@ -45,14 +50,12 @@ def intersectionPoints(nLabels, labels, root):
         cluster[numpy.where(labels == label)] = 1
         clusterSize = numpy.sum(cluster)
         points = list()
-        for i in range(1, width - 1):
-            for j in range(1, height - 1):
+        for i in range(1, height - 1):
+            for j in range(1, width - 1):
                 if cluster[i, j]:
                     points.append([i, j])
 
-        if clusterSize > 5:
-            if clusterSize == 8:
-                a = 0
+        if clusterSize > 5 or len(numpy.unique([p[0] for p in points])) > 3 or len(numpy.unique([p[1] for p in points])) > 3:
             if len(numpy.unique([p[0] for p in points])) > len(numpy.unique([p[1] for p in points])):
                 max = numpy.max([p[0] for p in points])
                 min = numpy.min([p[0] for p in points])
@@ -126,65 +129,128 @@ def intersectionIndex(point, pointsPerLabel):
     return index
 
 
-def nextNode(point, visited, label, endPoints, interPoints, nodes, connections, lastNode):
+def getNearestIntersectionPoint(point, intersectionPoints):
+    winDist = numpy.linalg.norm(numpy.array(intersectionPoints[0])-numpy.array(point[:]))
+    index = 0
+    for i in range(1, len(intersectionPoints)):
+        dist = numpy.linalg.norm(numpy.array(intersectionPoints[i])-numpy.array(point))
+        if dist < winDist:
+            winDist = dist
+            index = i
+    return intersectionPoints[index]
+
+
+def nextNode(point, visited, label, endPoints, interPoints, nodes, connections, lengths, lastNode):
+    walked = 0
     while True:
         if point in endPoints:
             nodes.append(point)
             connections.append([nodes.index(lastNode), len(nodes) - 1])
+            lengths.append(walked)
             print('Punto final')
             return
         else:
-            n = neighboursCoors(point[0], point[1])
-            if point in interPoints:
-                flag = point not in nodes
-                if flag:
-                    nodes.append(point)
-                connections.append([nodes.index(lastNode), len(nodes) - 1])
-                print('Interseccion')
-                if flag:
-                    nextPoints = list()
-                    for i in range(0,8,2):
-                        if label[n[i][0], n[i][1]] and not n[i] in visited:
-                            nextPoints.append(n[i])
-                            visited.append(n[i])
-                    for i in range(len(nextPoints)):
-                        print('Recursiva')
-                        visited.append(point)
-                        nextNode(nextPoints[i], visited, label, endPoints, interPoints, nodes, connections, point)
-                    nextPoints = list()
-                    for i in range(1,8,2):
-                        if label[n[i][0], n[i][1]] and not n[i] in visited:
-                            nextPoints.append(n[i])
-                            visited.append(n[i])
-                    for i in range(len(nextPoints)):
-                        print('Recursiva')
-                        visited.append(point)
-                        nextNode(nextPoints[i], visited, label, endPoints, interPoints, nodes, connections, point)
-                    return
+            if not point:
+                nearestInterPoint = getNearestIntersectionPoint(visited[-1], interPoints)
+                connections.append([nodes.index(lastNode), nodes.index(nearestInterPoint)])
+                lengths.append(walked)
+                return
             else:
-                # en el label 1 con el loop al tratar de crear el ultimo edge se tiene que pasar por un pixel visitado, entonces no ecuentra nada y falla en la siguiente vuelta
-                visited.append(point)
-                nextPoint = list()
-                if label[n[0][0], n[0][1]] and not n[0] in visited: nextPoint = n[0]
-                if label[n[2][0], n[2][1]] and not n[2] in visited: nextPoint = n[2]
-                if label[n[4][0], n[4][1]] and not n[4] in visited: nextPoint = n[4]
-                if label[n[6][0], n[6][1]] and not n[6] in visited: nextPoint = n[6]
-                if len(nextPoint) == 0:
-                    if label[n[1][0], n[1][1]] and not n[1] in visited: nextPoint = n[1]
-                    if label[n[3][0], n[3][1]] and not n[3] in visited: nextPoint = n[3]
-                    if label[n[5][0], n[5][1]] and not n[5] in visited: nextPoint = n[5]
-                    if label[n[7][0], n[7][1]] and not n[7] in visited: nextPoint = n[7]
-                point = nextPoint
+                n = neighboursCoors(point[0], point[1])
+                if point in interPoints:
+                    flag = point not in nodes
+                    if flag:
+                        nodes.append(point)
+                    connections.append([nodes.index(lastNode), len(nodes) - 1])
+                    lengths.append(walked)
+                    print('Interseccion')
+                    if flag:
+                        nextPoints = list()
+                        for i in range(0,8,2):
+                            if label[n[i][0], n[i][1]] and not n[i] in visited:
+                                nextPoints.append(n[i])
+                                visited.append(n[i])
+                        for i in range(len(nextPoints)):
+                            print('Recursiva')
+                            visited.append(point)
+                            nextNode(nextPoints[i], visited, label, endPoints, interPoints, nodes, connections, lengths, point)
+                        nextPoints = list()
+                        for i in range(1,8,2):
+                            if label[n[i][0], n[i][1]] and not n[i] in visited:
+                                nextPoints.append(n[i])
+                                visited.append(n[i])
+                        for i in range(len(nextPoints)):
+                            print('Recursiva')
+                            visited.append(point)
+                            nextNode(nextPoints[i], visited, label, endPoints, interPoints, nodes, connections, lengths, point)
+                        return
+                else:
+                    visited.append(point)
+                    nextPoint = list()
+                    if label[n[0][0], n[0][1]] and not n[0] in visited: nextPoint = n[0]
+                    if label[n[2][0], n[2][1]] and not n[2] in visited: nextPoint = n[2]
+                    if label[n[4][0], n[4][1]] and not n[4] in visited: nextPoint = n[4]
+                    if label[n[6][0], n[6][1]] and not n[6] in visited: nextPoint = n[6]
+                    if len(nextPoint) == 0:
+                        if label[n[1][0], n[1][1]] and not n[1] in visited: nextPoint = n[1]
+                        if label[n[3][0], n[3][1]] and not n[3] in visited: nextPoint = n[3]
+                        if label[n[5][0], n[5][1]] and not n[5] in visited: nextPoint = n[5]
+                        if label[n[7][0], n[7][1]] and not n[7] in visited: nextPoint = n[7]
+                    point = nextPoint
+                    walked += 1
+
+
+def makeGraph(roots):
+    width, height = roots.shape
+    endPoints = getEndPoints(roots)
+    intsctl = list()
+    for i in range(1, width - 1):
+        for j in range(1, height - 1):
+            if roots[i, j] and transitions(notSoNeighbours(i, j, roots)) > 2 and numpy.sum(neighbours(i, j, roots)) > 2:
+                intsctl.append((i, j))
+    intersect = numpy.zeros_like(labels)
+    for index in range(len(intsctl)):
+        intersect[intsctl[index][0], intsctl[index][1]] = 1
+    nintlabel, intlabels, intstats, centroids = cv2.connectedComponentsWithStats(numpy.uint8(intersect))
+
+    interPoints = intersectionPoints(nintlabel, intlabels, roots)
+
+    pointsPerLabel = list()
+    for label in range(1, nintlabel):
+        pointsPerLabel.append(numpy.where(intlabels == label))
+    initPoint = getHighestEndPoint(endPoints)
+    endPoints.remove(initPoint)
+    visited = deque()
+    nodes = list()
+    connections = list()
+    lengths = list()
+    nodes.append(initPoint)
+    nextNode(initPoint, visited, roots, endPoints, interPoints, nodes, connections, lengths, initPoint)
+    return nodes, connections, lengths
+
+
+def drawGraph(canvas, nodes, connections):
+    for edge in connections:
+        p1 = tuple(reversed(nodes[edge[0]]))
+        p2 = tuple(reversed(nodes[edge[1]]))
+        cv2.line(canvas, p1, p2, (255, 255, 255), 1)
+        cv2.circle(canvas, p1, 2, (0, 0, 255), -1)
+        cv2.circle(canvas, p2, 2, (0, 0, 255), -1)
+    return canvas
+
 
 filename = sys.argv[1]
 img = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
-# img[408:415, 250:253] = 0
-# img[316:318, 271:273] = 0
-# img[367:369, 353:355] = 0
-# img[374:378, 349:352] = 0
-height, width = img.shape
+file = os.path.split(filename)
+root = file[0]
+root = os.path.dirname(root)
+file = file[1]
+file = os.path.splitext(file)
+file = file[0]
+
+
 nlabels, labels, stats, centroids = cv2.connectedComponentsWithStats(numpy.uint8(img))
-largestLabels = numpy.argpartition(stats[1:-1,-1], -3)[-3:] + 1
+largestLabels = numpy.argpartition(stats[1:, -1], -3)[-3:] + 1
 label1 = numpy.zeros_like(labels)
 label2 = numpy.zeros_like(labels)
 label3 = numpy.zeros_like(labels)
@@ -195,66 +261,49 @@ label2 = numpy.uint8(label2)
 label3[numpy.where(labels == largestLabels[2])] = 1
 label3 = numpy.uint8(label3)
 
-roots = label3
-# roots[319:323, 197:199] = 0
+nodes1, connections1, lengths1 = makeGraph(label1)
+nodes2, connections2, lengths2 = makeGraph(label2)
+nodes3, connections3, lengths3 = makeGraph(label3)
 
-endPoints = getEndPoints(roots)
-intsctl = list()
-for i in range(1, width - 1):
-    for j in range(1, height - 1):
-        if roots[i, j] and transitions(notSoNeighbours(i, j, roots)) > 2 and numpy.sum(neighbours(i, j, roots)) > 2:
-            intsctl.append((i, j))
-intersect = numpy.zeros_like(labels)
-for index in range(len(intsctl)):
-    intersect[intsctl[index][0], intsctl[index][1]] = 1
-nintlabel, intlabels, intstats, centroids = cv2.connectedComponentsWithStats(numpy.uint8(intersect))
+width, height = img.shape
+graph = numpy.zeros((width, height, 3), numpy.uint8)
 
-interPoints = intersectionPoints(nintlabel, intlabels, roots)
+graph = drawGraph(graph, nodes1, connections1)
+graph = drawGraph(graph, nodes2, connections2)
+graph = drawGraph(graph, nodes3, connections3)
 
-# pointsPerLabel = list()
-# for label in range(1,nintlabel):
-#     pointsPerLabel.append(numpy.where(intlabels == label))
-# initPoint = getHighestEndPoint(endPoints)
-# endPoints.remove(initPoint)
-# visited = deque()
-# nodes = list()
-# connections = list()
-# nodes.append(initPoint)
-# nextNode(initPoint, visited, roots, endPoints, interPoints, nodes, connections, initPoint)
-# print(nodes)
-# print(connections)
+graphpath = os.path.join('graphs/', file + '_graph.png')
+graphpath = os.path.join(root, graphpath)
 
-blank = 255*numpy.ones_like(roots)
-label_hue = numpy.uint8(179*(roots + intersect*5))
-for index in range(len(interPoints)):
-    label_hue[interPoints[index][0], interPoints[index][1]] = 120
-for index in range(len(endPoints)):
-    label_hue[endPoints[index][0], endPoints[index][1]] = 50
-withintersect = cv2.merge([label_hue, blank, blank])
-withintersect = cv2.cvtColor(withintersect, cv2.COLOR_HSV2BGR)
-withintersect[roots == 0] = 0
+cv2.imwrite(graphpath, graph, (cv2.IMWRITE_PNG_COMPRESSION, 0))
 
-# # Dibuja el grafo sobre la imagen
-# for edge in connections:
-#     p1 = tuple(reversed(nodes[edge[0]]))
-#     p2 = tuple(reversed(nodes[edge[1]]))
-#     cv2.line(withintersect, p1, p2, (255, 255, 255), 1)
-#     cv2.circle(withintersect, p1, 1, (100, 100, 100))
-#     cv2.circle(withintersect, p2, 1, (100, 100, 100))
-
-cv2.imshow('intersect',withintersect)
-
-## Esta parte muestra todas las labels en distintos colores
-# label_hue = numpy.uint8(179*labels/numpy.max(labels))
-# blank_ch = 255*numpy.ones_like(label_hue)
-# labeled_img = cv2.merge([label_hue, blank_ch, blank_ch])
+# system('clear')
+# print('Graph 1:')
+# print('------------------------------------------')
+# print('Nodes:')
+# print(nodes1)
+# print('Connections:')
+# print(connections1)
+# print('Lengths:')
+# print(lengths1)
+# print('')
+# print('Graph 2:')
+# print('------------------------------------------')
+# print('Nodes:')
+# print(nodes2)
+# print('Connections:')
+# print(connections2)
+# print('Lengths:')
+# print(lengths2)
+# print('')
+# print('Graph 3:')
+# print('------------------------------------------')
+# print('Nodes:')
+# print(nodes3)
+# print('Connections:')
+# print(connections3)
+# print('Lengths:')
+# print(lengths3)
 #
-# # cvt to BGR for display
-# labeled_img = cv2.cvtColor(labeled_img, cv2.COLOR_HSV2BGR)
-#
-# # set bg label to black
-# labeled_img[label_hue==0] = 0
-#
-# cv2.imshow('labeled.png', labeled_img)
-
-cv2.waitKey()
+# cv2.imshow('Graphs', graph)
+# cv2.waitKey()
--- skeletonize.py
+++ skeletonize.py
@@ -3,15 +3,14 @@
 import cv2
 import sys
 import numpy
-import math
 import os.path
 
 def neighbours(x,y,image):
     "Return 8-neighbours of image point P1(x,y), in a clockwise order"
-    img = image
+    # img = image
     x_1, y_1, x1, y1 = x-1, y-1, x+1, y+1
-    return [ img[x_1][y], img[x_1][y1], img[x][y1], img[x1][y1],     # P2,P3,P4,P5
-                img[x1][y], img[x1][y_1], img[x][y_1], img[x_1][y_1] ]    # P6,P7,P8,P9
+    return [ image[x_1][y], image[x_1][y1], image[x][y1], image[x1][y1],     # P2,P3,P4,P5
+             image[x1][y], image[x1][y_1], image[x][y_1], image[x_1][y_1] ]    # P6,P7,P8,P9
 
 def transitions(neighbours):
     "No. of 0,1 patterns (transitions from 0 to 1) in the ordered sequence"
@@ -19,15 +18,15 @@ def transitions(neighbours):
     return sum( (n1, n2) == (0, 1) for n1, n2 in zip(n, n[1:]) )  # (P2,P3), (P3,P4), ... , (P8,P9), (P9,P2)
 
 def fillblanks(image):
-    img = image
-    height, width = img.shape
-    for i in range(1, width-1):
-        for j in range(1, height-1):
-            if (img[i,j] == 0):
-                n = neighbours(i,j,img)
+    # img = image
+    height, width = image.shape
+    for i in range(1, height-1):
+        for j in range(1, width-1):
+            if (image[i,j] == 0):
+                n = neighbours(i,j,image)
                 if (numpy.sum(n) > 5):
-                    img[i,j] = 1
-    return img
+                    image[i,j] = 1
+    return image
 
 
 def skeletonize(image):
@@ -65,18 +64,59 @@ def skeletonize(image):
             Image_Thinned[x][y] = 0
     return Image_Thinned
 
-def make_skeleton(input, output):
-    img = cv2.imread(input, cv2.IMREAD_GRAYSCALE)
-    height, width = img.shape
-    cv2.imshow('asasd',img)
-    skeleton = skeletonize(img/255)
-    cv2.imwrite(output, img, (cv2.IMWRITE_PNG_COMPRESSION, 0))
-
 
 filename = sys.argv[1]
 img = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
-height, width = img.shape
-skeleton = skeletonize(img/255)
-cv2.imwrite('simpleskeleton2.png', skeleton, (cv2.IMWRITE_PNG_COMPRESSION, 0))
-cv2.imshow('skeleton',skeleton)
-cv2.waitKey()
+file = os.path.split(filename)
+root = file[0]
+root = os.path.dirname(root)
+file = file[1]
+file = os.path.splitext(file)
+file = file[0]
+
+mask1 = os.path.join('masks1/', file + '_mask1.png')
+mask1 = os.path.join(root, mask1)
+mask2 = os.path.join('masks2/', file + '_mask2.png')
+mask2 = os.path.join(root, mask2)
+mask3 = os.path.join('masks3/', file + '_mask3.png')
+mask3 = os.path.join(root, mask3)
+mask1 = cv2.imread(mask1, cv2.IMREAD_GRAYSCALE)
+mask2 = cv2.imread(mask2, cv2.IMREAD_GRAYSCALE)
+mask3 = cv2.imread(mask3, cv2.IMREAD_GRAYSCALE)
+mask1points = cv2.findNonZero(mask1)
+mask2points = cv2.findNonZero(mask2)
+mask3points = cv2.findNonZero(mask3)
+mask1rect = cv2.boundingRect(mask1points)
+mask2rect = cv2.boundingRect(mask2points)
+mask3rect = cv2.boundingRect(mask3points)
+
+segm1 = os.path.join('segm/', file + '_segm1.png')
+segm1 = os.path.join(root, segm1)
+segm2 = os.path.join('segm/', file + '_segm2.png')
+segm2 = os.path.join(root, segm2)
+segm3 = os.path.join('segm/', file + '_segm3.png')
+segm3 = os.path.join(root, segm3)
+segm1 = cv2.imread(segm1, cv2.IMREAD_GRAYSCALE)
+segm2 = cv2.imread(segm2, cv2.IMREAD_GRAYSCALE)
+segm3 = cv2.imread(segm3, cv2.IMREAD_GRAYSCALE)
+segm1 = cv2.bitwise_and(segm1[mask1rect[1] - 20:mask1rect[1] + mask1rect[3] + 20, mask1rect[0] - 20:mask1rect[0] + mask1rect[2] + 20], mask1[mask1rect[1] - 20:mask1rect[1] + mask1rect[3] + 20, mask1rect[0] - 20:mask1rect[0] + mask1rect[2] + 20])
+segm2 = cv2.bitwise_and(segm2[mask2rect[1] - 20:mask2rect[1] + mask2rect[3] + 20, mask2rect[0] - 20:mask2rect[0] + mask2rect[2] + 20], mask2[mask2rect[1] - 20:mask2rect[1] + mask2rect[3] + 20, mask2rect[0] - 20:mask2rect[0] + mask2rect[2] + 20])
+segm3 = cv2.bitwise_and(segm3[mask3rect[1] - 20:mask3rect[1] + mask3rect[3] + 20, mask3rect[0] - 20:mask3rect[0] + mask3rect[2] + 20], mask3[mask3rect[1] - 20:mask3rect[1] + mask3rect[3] + 20, mask3rect[0] - 20:mask3rect[0] + mask3rect[2] + 20])
+
+skele1 = skeletonize(segm1/255)
+print('Skeletonization 1')
+skele2 = skeletonize(segm2/255)
+print('Skeletonization 2')
+skele3 = skeletonize(segm3/255)
+print('Skeletonization 3')
+
+skelepath1 = os.path.join('skeleton/', file + '_skele1.png')
+skelepath1 = os.path.join(root, skelepath1)
+skelepath2 = os.path.join('skeleton/', file + '_skele2.png')
+skelepath2 = os.path.join(root, skelepath2)
+skelepath3 = os.path.join('skeleton/', file + '_skele3.png')
+skelepath3 = os.path.join(root, skelepath3)
+
+cv2.imwrite(skelepath1, skele1*255, (cv2.IMWRITE_PNG_COMPRESSION, 0))
+cv2.imwrite(skelepath2, skele2*255, (cv2.IMWRITE_PNG_COMPRESSION, 0))
+cv2.imwrite(skelepath3, skele3*255, (cv2.IMWRITE_PNG_COMPRESSION, 0))
